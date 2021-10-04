60dd163edc2af25166e044e44abc326145c1d7c1
Berra <hbberra@gmail.com>
2021-10-04 02:06:57 +0200

    berra-web

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,16 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import Layout from './Layout';
 import { getProducts } from './apiCore';
-import { Col } from 'react-bootstrap'
-import { Link } from 'react-router-dom'
+import { Col} from 'react-bootstrap'
+
 import Product from './Card';
-import { Carousel } from 'react-bootstrap'
-import ShowImage from './ShowImage';
+
+
 
 
 export default function Home(url,item) {
     const [productsBySell, setProductsBySell] = useState([]);
-    const [productsByArrival, setProductsByArrival] = useState([]);
+    const [, setProductsByArrival] = useState([]);
     const [, setError] = useState(false);
 
     const loadProductsBySell = () => {
@@ -43,34 +43,7 @@ export default function Home(url,item) {
             className="container-fluid">
 
             <h2 className="mb-4">New Arrivals</h2>
-          <Carousel fade controls={false} indicators interval={3000} pause={false} className='bg-dark'>
-          {productsByArrival.map((product, i) => (
-            <Carousel.Item key={product.id}>
-              <Link to={`/product/${product.id}`}>
-                <ShowImage  item={product} url="product" />
-                <Carousel.Caption className='carousel-caption'>
-                  <h2>
-                    {product.name} (${product.price})
-                  </h2>
-                </Carousel.Caption>
-              </Link>
-            </Carousel.Item>
-          )
-          )}
-        
-      
-        </Carousel>
-
-            {/* <div className="row">
-                {productsByArrival.map((product, i) =>
-                (
-                <Col sm={12} md={6} lg={4} xl={3}>
-                    <Product key={i} product={product} />
-                    </Col>
-                ))}
-            </div> */}
-
-            <h2 className="mb-4">Best Sellers</h2>
+         
             <div className="row">
                 {productsBySell.map((product, i) =>
                 (
